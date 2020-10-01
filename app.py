fd701d9baade36f9ea5f9ec9c67c34c8fcffdb68
AhmedAlsudairi <AhmedAlsudairi@users.noreply.github.com>
2020-10-01 13:15:04 +0300

    connecting to the database and fetching the data

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
-
+from  flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:admin@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+class ToDo(db.Model):
+    __tablename__ = 'todos'
+    id= db.Column(db.Integer,primary_key=True)
+    describtion=db.Column(db.String(),nullable=False)
+
+    def __repr__(self):
+        return '<Item {self.id} {self.describtion}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html',data= [
-        { 'description': 'To do 1'},
-        { 'description': 'To do 2'},
-        { 'description': 'To do 3'}
-    ])
+    return render_template('index.html',data=ToDo.query.all())
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()        
